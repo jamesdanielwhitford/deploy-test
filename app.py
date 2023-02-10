5a979ad567b9945e740bab48a1453d09101de52d
jamesdanielwhitford <james.whitford0@gmail.com>
2023-02-10 20:16:54 +0200

    Update app with postgres

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+import psycopg2
 from flask import Flask, render_template, request
+
+# setup environment variables
+import os
 
 app = Flask(__name__)
 
@@ -6,10 +10,26 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/hello', methods=['POST'])
-def hello():
+@app.route('/add-name', methods=['POST'])
+def add_name():
     name = request.form['name']
-    return render_template('hello.html', name=name)
+    connection = psycopg2.connect(os.environ.get("DATABASE_URL"))
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO users (name) VALUES (%s)", (name,))
+    connection.commit()
+    connection.close()
+    return render_template('add-name-success.html', name=name)
+
+
+@app.route('/names')
+def names():
+    connection = psycopg2.connect(os.environ.get("DATABASE_URL"))
+    cursor = connection.cursor()
+    cursor.execute("SELECT name from users")
+    rows = cursor.fetchall()
+    connection.close()
+    return render_template('names.html', rows=rows)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
