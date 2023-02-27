a69507d2184cbde6e8318c53c1fddfeab01526fd
jamesdanielwhitford <james.whitford0@gmail.com>
2023-02-27 10:27:58 +0200

    Remove crud for now

--- app.py
+++ app.py
@@ -19,34 +19,34 @@ def index():
 def randle():
     return render_template('randle.html')
 
-@app.route('/add-name', methods=['POST'])
-def add_name():
-    name = request.form['name']
-    connection = psycopg2.connect(os.environ.get("DATABASE_URL"))
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO users (name) VALUES (%s)", (name,))
-    connection.commit()
-    connection.close()
-    return render_template('add-name-success.html', name=name)
+# @app.route('/add-name', methods=['POST'])
+# def add_name():
+#     name = request.form['name']
+#     connection = psycopg2.connect(os.environ.get("DATABASE_URL"))
+#     cursor = connection.cursor()
+#     cursor.execute("INSERT INTO users (name) VALUES (%s)", (name,))
+#     connection.commit()
+#     connection.close()
+#     return render_template('add-name-success.html', name=name)
 
-@app.route('/delete-name', methods=['POST'])
-def delete_name():
-    name = request.form['name']
-    connection = psycopg2.connect(os.environ.get("DATABASE_URL"))
-    cursor = connection.cursor()
-    cursor.execute("DELETE FROM users WHERE name = %s", (name,))
-    connection.commit()
-    connection.close()
-    return render_template('add-name-success.html', name=name)
+# @app.route('/delete-name', methods=['POST'])
+# def delete_name():
+#     name = request.form['name']
+#     connection = psycopg2.connect(os.environ.get("DATABASE_URL"))
+#     cursor = connection.cursor()
+#     cursor.execute("DELETE FROM users WHERE name = %s", (name,))
+#     connection.commit()
+#     connection.close()
+#     return render_template('add-name-success.html', name=name)
 
-@app.route('/names')
-def names():
-    connection = psycopg2.connect(os.environ.get("DATABASE_URL"))
-    cursor = connection.cursor()
-    cursor.execute("SELECT name from users")
-    rows = cursor.fetchall()
-    connection.close()
-    return render_template('names.html', rows=rows)
+# @app.route('/names')
+# def names():
+#     connection = psycopg2.connect(os.environ.get("DATABASE_URL"))
+#     cursor = connection.cursor()
+#     cursor.execute("SELECT name from users")
+#     rows = cursor.fetchall()
+#     connection.close()
+#     return render_template('names.html', rows=rows)
 
 
 if __name__ == '__main__':
