1cc389f5ab5733b7f585f53751ce2472192dc408
jamesdanielwhitford <70632508+jamesdanielwhitford@users.noreply.github.com>
2024-09-01 10:44:27 +0200

    Cache busting and versioning implemented

--- app.py
+++ app.py
@@ -1,62 +1,36 @@
-# import psycopg2
-from flask import Flask, render_template, request
-# from flask_session import Session;
-from flask import session
-from flask import jsonify
-from datetime import date
+// In your app.py file
 
-
-# setup environment variables
 import os
-
-# Development
-# from dotenv import load_dotenv
-# load_dotenv()
-
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
-# Session(app)
+# Get the current version from an environment variable or a file
+VERSION = os.environ.get('APP_VERSION', '1.0.0')
+
+@app.after_request
+def add_header(response):
+    # Prevent caching for HTML files
+    if response.content_type.startswith('text/html'):
+        response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+        response.headers['Pragma'] = 'no-cache'
+        response.headers['Expires'] = '0'
+    return response
 
 @app.route('/')
 def index():
-    return render_template('hardle.html')
+    return render_template('hardle.html', version=VERSION)
 
 @app.route('/randle')
 def randle():
-    return render_template('randle.html')
+    return render_template('randle.html', version=VERSION)
 
-
-
-# @app.route('/add-name', methods=['POST'])
-# def add_name():
-#     name = request.form['name']
-#     connection = psycopg2.connect(os.environ.get("DATABASE_URL"))
-#     cursor = connection.cursor()
-#     cursor.execute("INSERT INTO users (name) VALUES (%s)", (name,))
-#     connection.commit()
-#     connection.close()
-#     return render_template('add-name-success.html', name=name)
-
-# @app.route('/delete-name', methods=['POST'])
-# def delete_name():
-#     name = request.form['name']
-#     connection = psycopg2.connect(os.environ.get("DATABASE_URL"))
-#     cursor = connection.cursor()
-#     cursor.execute("DELETE FROM users WHERE name = %s", (name,))
-#     connection.commit()
-#     connection.close()
-#     return render_template('add-name-success.html', name=name)
-
-# @app.route('/names')
-# def names():
-#     connection = psycopg2.connect(os.environ.get("DATABASE_URL"))
-#     cursor = connection.cursor()
-#     cursor.execute("SELECT name from users")
-#     rows = cursor.fetchall()
-#     connection.close()
-#     return render_template('names.html', rows=rows)
-
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    response = send_from_directory('static', filename)
+    # Set cache control for static assets (1 year)
+    response.headers['Cache-Control'] = 'public, max-age=31536000'
+    return response
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
